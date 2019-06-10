a772053687dc97cf808a91e2503ca94318205257
xiaopf <326258842@qq.com>
2019-06-10 23:56:49 +0800

    完成about us 页面

--- src/entry/aboutUs.js
+++ src/entry/aboutUs.js
@@ -1,1 +1,10 @@
-console.log('webpack 打包！')
+$(document).ready(function(){
+    $('.page_tab_nav li').click(function () {
+        $('.page_tab_nav li').removeClass('active');
+        $('.page_tab_content li').removeClass('active')
+        
+        let index = $('.page_tab_nav li').index(this);
+        $('.page_tab_content li').eq(index).addClass('active')
+        $(this).addClass('active');
+    })
+})
